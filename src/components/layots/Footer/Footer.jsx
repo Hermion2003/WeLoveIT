5e6a9877e9a08696aa8ce66025188a4a33ba9814
Hermion2003 <ofwork2003@gmail.com>
2025-06-05 18:17:56 +0200

    Add files via upload

--- src/components/layots/Footer/Footer.jsx
+++ src/components/layots/Footer/Footer.jsx
@@ -18,38 +18,40 @@ const Footer = () => {
                                 </h3>
                             </div>
                         </div>
+                        <div className="containerForColumns">
+                            <div className="column">
+                                <h4>Курси</h4>
+                                <ul>
+                                    <li>Тестувальник програмного забезпечення (QA)</li>
+                                    <li>Python розробник</li>
+                                    <li>Front-end розробник</li>
+                                    <li>Full-stack розробник</li>
+                                    <li>Java розробник</li>
+                                </ul>
+                            </div>
 
-                        <div className="column">
-                            <h4>Курси</h4>
-                            <ul>
-                                <li>Тестувальник програмного забезпечення (QA)</li>
-                                <li>Python розробник</li>
-                                <li>Front-end розробник</li>
-                                <li>Full-stack розробник</li>
-                                <li>Java розробник</li>
-                            </ul>
+                            <div className="column">
+                                <h4>Відкрийте більше</h4>
+                                <ul>
+                                    <li>Формати навчання</li>
+                                    <li>Про нас</li>
+                                    <li>Оголошення</li>
+                                    <li>Кар'єра</li>
+                                    <li>Ми наймаємо</li>
+                                </ul>
+                            </div>
+
+                            <div className="column">
+                                <h4>Файли для завантаження</h4>
+                                <ul>
+                                    <li>iOS</li>
+                                    <li>Android</li>
+                                </ul>
+                            </div>
                         </div>
 
-                        <div className="column">
-                            <h4>Відкрийте більше</h4>
-                            <ul>
-                                <li>Формати навчання</li>
-                                <li>Про нас</li>
-                                <li>Оголошення</li>
-                                <li>Кар'єра</li>
-                                <li>Ми наймаємо</li>
-                            </ul>
-                        </div>
-
-                        <div className="column">
-                            <h4>Файли для завантаження</h4>
-                            <ul>
-                                <li>iOS</li>
-                                <li>Android</li>
-                            </ul>
-                        </div>
                         <div className="biggerColumn">
-                            <button className={"lightBtn"}>Зареєструватися</button>
+                            <button className={"lightBtn"}>Реєстрація</button>
                             <button className={"darkBtn"}>Увійти</button>
                         </div>
                     </div>
--- src/components/layots/Header/Nav/Nav.jsx
+++ src/components/layots/Header/Nav/Nav.jsx
@@ -1,53 +1,78 @@
 import "./Nav.scss";
-import { FaPhoneFlip, FaRegCircleUser } from "react-icons/fa6";
+import {FaPhoneFlip, FaRegCircleUser, FaX} from "react-icons/fa6";
 import logo from "./../../../../assets/jobLogo.svg";
-import { TiArrowSortedDown } from "react-icons/ti";
+import {TiArrowSortedDown} from "react-icons/ti";
 import ConsultationModal from "../../../consultationModal/ConsultationModal.jsx";
+import {useEffect, useState} from "react";
+import {FiMenu} from "react-icons/fi";
 
 const Nav = () => {
-  return (
-    <nav className={"nav"}>
-      <img src={logo} alt="logo" className="logoStyleNav" />
-      <div className={"navInCenterOfNav"}>
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 1000);
+
+    const menu = (<div className={"navInCenterOfNav"}>
         <div className={"blockForItemNav"}>
-          <a href="#" className={"linkInNav"}>
-            Вакансії
-          </a>
-          <TiArrowSortedDown className={"iconInItemNav"} />
+            <a href="#" className={"linkInNav"}>
+                Вакансії
+            </a>
+            <TiArrowSortedDown className={"iconInItemNav"}/>
         </div>
         <div className={"blockForItemNav"}>
-          <a href="#" className={"linkInNav"}>
-            Про компанію
-          </a>
-          <TiArrowSortedDown className={"iconInItemNav"} />
+            <a href="#" className={"linkInNav"}>
+                Про компанію
+            </a>
+            <TiArrowSortedDown className={"iconInItemNav"}/>
         </div>
         <div className={"blockForItemNav"}>
-          <a href="#" className={"linkInNav"}>
-            Відгуки
-          </a>
-          <TiArrowSortedDown className={"iconInItemNav"} />
+            <a href="#" className={"linkInNav"}>
+                Відгуки
+            </a>
+            <TiArrowSortedDown className={"iconInItemNav"}/>
         </div>
         <div className={"blockForItemNav"}>
-          <a href="#" className={"linkInNav"}>
-            Акції
-          </a>
-          <TiArrowSortedDown className={"iconInItemNav"} />
+            <a href="#" className={"linkInNav"}>
+                Акції
+            </a>
+            <TiArrowSortedDown className={"iconInItemNav"}/>
         </div>
         <div className={"blockForItemNav"}>
-          <a href="#" className={"linkInNav"}>
-            Контакти
-          </a>
-          <TiArrowSortedDown className={"iconInItemNav"} />
+            <a href="#" className={"linkInNav"}>
+                Контакти
+            </a>
+            <TiArrowSortedDown className={"iconInItemNav"}/>
         </div>
-      </div>
-      <div className={"rightPartOfNav"}>
-        <FaPhoneFlip className={"styleIconNav"} />
-        <FaRegCircleUser className={"styleIconNav"} />
-        <ConsultationModal>
-          <button className={"consultationBtn"}>Отримати консультацію</button>
-        </ConsultationModal>
-      </div>
-    </nav>
-  );
+    </div>)
+
+    return (
+        <nav className={"nav"}>
+            <img src={logo} alt="logo" className="logoStyleNav"/>
+            <div className={"normalMenu"}>
+                {menu}
+            </div>
+
+
+            <div className={"rightPartOfNav"}>
+                <FaPhoneFlip className={"styleIconNav"}/>
+                <FaRegCircleUser className={"styleIconNav"}/>
+                <ConsultationModal>
+                    <button className={"consultationBtn"}>Отримати консультацію</button>
+                </ConsultationModal>
+            </div>
+         <FiMenu onClick={()=>setIsMobile(!isMobile)} className={"hamburger"}/>
+            <div className={`mobileMenu ${isMobile?" isOpen":""}`}>
+                <div className={"topMenuMobile"}>
+
+                <FaX onClick={()=>setIsMobile(false)}/>
+                </div>
+                <div className={"middle"}>
+
+                {menu}
+                </div>
+                <div className={"bottom"}>
+                    <FaPhoneFlip className={"styleIconNav"}/>
+                    <FaRegCircleUser className={"styleIconNav"}/>
+                </div>
+            </div>
+        </nav>
+    );
 };
 export default Nav;
